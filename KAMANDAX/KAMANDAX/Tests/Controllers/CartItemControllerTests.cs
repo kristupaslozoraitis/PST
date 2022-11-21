c199264cb997a5e5ba6f7d2eeab7039bd6ca5e90
erikasnar <naerikas@gmail.com>
2022-11-21 20:07:03 +0200

    Updating tests

--- KAMANDAX/KAMANDAX/Tests/Controllers/CartItemControllerTests.cs
+++ KAMANDAX/KAMANDAX/Tests/Controllers/CartItemControllerTests.cs
@@ -6,48 +6,79 @@ namespace Tests.Controllers
 {
     public class CartItemControllerTests
     {
-        private Mock<ICartItemService> mockCartItemService;
         private Fixture fixture;
 
         public CartItemControllerTests()
         {
-            mockCartItemService = new Mock<ICartItemService>();
             fixture = new Fixture();
         }
 
-        private CartItemController CreateCartItemController()
+        private static WebDbContext CreateInMemoryWebDbContext()
         {
-            return new CartItemController(mockCartItemService.Object);
+            var optionsBuilder = new DbContextOptionsBuilder<WebDbContext>()
+                            .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var context = new WebDbContext(optionsBuilder.Options);
+            return context;
+        }
+
+        private CartItemController CreateCartItemController(ICartItemService cartItemService)
+        {
+            return new CartItemController(cartItemService);
         }
 
         [Fact]
-        public async Task AddCartItem_StateUnderTest_ExpectedBehavior()
+        public async Task AddCartItem_MultipleCartItemsWithSameProductIdExist_Status200Response()
         {
-            var cartItemController = CreateCartItemController();
+            WebDbContext context = CreateInMemoryWebDbContext();
+            ICartItemService cartItemService = new CartItemService(context);
+            var cartItemController = CreateCartItemController(cartItemService);
             Product product = fixture.Create<Product>();
             Guid userId = new Guid();
 
-            mockCartItemService.Setup(x => x.GetUserCartItems(It.IsAny<Guid>())).ReturnsAsync(new List<CartItem>());
             var result = await cartItemController.AddCartItem(product, userId);
 
-            mockCartItemService.Verify(x => x.Create(It.IsAny<CartItem>()), Times.Once);
             var okResult = Assert.IsType<OkResult>(result);
             Assert.NotNull(okResult);
             Assert.Equal(200, okResult.StatusCode);
         }
 
         [Fact]
-        public async Task DeleteCartItem_StateUnderTest_ExpectedBehavior()
+        public async Task AddCartItem_MultipleCartItemsWithSameProductIdExist_Status400Response()
         {
-            var cartItemController = CreateCartItemController();
+            Product product = fixture.Create<Product>();
+            Guid userId = new Guid();
+            CartItem firstCartItem = fixture.Create<CartItem>();
+            firstCartItem.ProductId = product.ProductId;
+            firstCartItem.ProductUserId = userId;
+            CartItem secondCartItem = fixture.Create<CartItem>();
+            secondCartItem.ProductId = product.ProductId;
+            secondCartItem.ProductUserId = userId;
+            WebDbContext context = CreateInMemoryWebDbContext();
+            await context.CartItems.AddAsync(firstCartItem);
+            await context.CartItems.AddAsync(secondCartItem);
+            await context.SaveChangesAsync();
+            ICartItemService cartItemService = new CartItemService(context);
+            var cartItemController = CreateCartItemController(cartItemService);
+
+            var result = await cartItemController.AddCartItem(product, userId);
+
+            var badRequestResult = Assert.IsType<BadRequestResult>(result);
+            Assert.NotNull(badRequestResult);
+            Assert.Equal(400, badRequestResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task DeleteCartItem_ItemDeletesSuccessfully_Status200Response()
+        {
             CartItem cartItem = fixture.Create<CartItem>();
+            WebDbContext context = CreateInMemoryWebDbContext();
+            await context.CartItems.AddAsync(cartItem);
+            await context.SaveChangesAsync();
+            ICartItemService cartItemService = new CartItemService(context);
+            var cartItemController = CreateCartItemController(cartItemService);
 
-            mockCartItemService.Setup(x => x.GetUserCartItems(It.IsAny<Guid>())).ReturnsAsync(new List<CartItem>());
+            var result = await cartItemController.DeleteCartItem(cartItem);
 
-            var result = await cartItemController.DeleteCartItem(
-                cartItem);
-
-            mockCartItemService.Verify(x => x.DeleteCartItem(It.IsAny<CartItem>()), Times.Once);
             var okResult = Assert.IsType<OkResult>(result);
             Assert.NotNull(okResult);
             Assert.Equal(200, okResult.StatusCode);
@@ -56,23 +87,23 @@ namespace Tests.Controllers
         [Fact]
         public async Task GetUserCartItems_StateUnderTest_ExpectedBehavior()
         {
-            var cartItemController = CreateCartItemController();
+            WebDbContext context = CreateInMemoryWebDbContext();
+            ICartItemService cartItemService = new CartItemService(context);
+            var cartItemController = CreateCartItemController(cartItemService);
             Guid userId = new Guid();
 
             var result = await cartItemController.GetUserCartItems(userId);
-
-            mockCartItemService.Verify(x => x.GetUserCartItems(userId), Times.Once);
         }
 
         [Fact]
         public async Task DeleteAllUserItems_StateUnderTest_ExpectedBehavior()
         {
-            var cartItemController = this.CreateCartItemController();
+            WebDbContext context = CreateInMemoryWebDbContext();
+            ICartItemService cartItemService = new CartItemService(context);
+            var cartItemController = this.CreateCartItemController(cartItemService);
             Guid userId = new Guid();
 
             await cartItemController.DeleteAllUserItems(userId);
-
-            mockCartItemService.Verify(x => x.DeleteAllUserItems(userId), Times.Once);
         }
     }
 }
