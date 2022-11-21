01d3f147c76ac18eb244595dae404c076025de3c
kristupaslozoraitis <65513238+kristupaslozoraitis@users.noreply.github.com>
2022-11-21 20:33:22 +0200

    Some parameterized tests

--- KAMANDAX/KAMANDAX/Tests/UserServiceTests.cs
+++ KAMANDAX/KAMANDAX/Tests/UserServiceTests.cs
@@ -2,8 +2,11 @@ namespace Tests
 {
     public class UserServiceTests
     {
-        [Fact]
-        public async void GetUserByEmail_UsersExists_ReturnedCorrectValue()
+        [Theory]
+        [InlineData("email1@email.com", "Test1")]
+        [InlineData("email2@email.com", "Test2")]
+        [InlineData("email3@email.com", "Test3")]
+        public async void GetUserByEmail_UsersExists_ReturnedCorrectValue(string email, string expectedName)
         {
             var options = new DbContextOptionsBuilder<WebDbContext>()
                 .UseInMemoryDatabase(databaseName: "kamandax")
@@ -14,8 +17,30 @@ namespace Tests
                 context.Users.Add(new User
                 {
                     Id = Guid.NewGuid(),
-                    FullName = "Test",
-                    Email = "email@email.com",
+                    FullName = "Test1",
+                    Email = "email1@email.com",
+                    Password = "Password",
+                    Address = "Address",
+                    Country = "LT",
+                    City = "City",
+                    PostalCode = "123456"
+                });
+                context.Users.Add(new User
+                {
+                    Id = Guid.NewGuid(),
+                    FullName = "Test2",
+                    Email = "email2@email.com",
+                    Password = "Password",
+                    Address = "Address",
+                    Country = "LT",
+                    City = "City",
+                    PostalCode = "123456"
+                });
+                context.Users.Add(new User
+                {
+                    Id = Guid.NewGuid(),
+                    FullName = "Test3",
+                    Email = "email3@email.com",
                     Password = "Password",
                     Address = "Address",
                     Country = "LT",
@@ -27,9 +52,40 @@ namespace Tests
             using(var context = new WebDbContext(options))
             {
             var service = new UserService(context);
-            var userNew = await service.GetUserByEmail("email@email.com");
+            var userNew = await service.GetUserByEmail(email);
 
-            Assert.Equal("email@email.com", userNew.Email);
+            Assert.Equal(expectedName, userNew.FullName);
+            }
+        }
+        [Fact]
+        public async void GetUserById_UsersExists_ReturnedCorrectValues()
+        {
+            var id = Guid.NewGuid();
+            var options = new DbContextOptionsBuilder<WebDbContext>()
+                .UseInMemoryDatabase(databaseName: "kamandax")
+                .Options;
+
+            using (var context = new WebDbContext(options))
+            {
+                context.Users.Add(new User
+                {
+                    Id = id,
+                    FullName = "Test1",
+                    Email = "email1@email.com",
+                    Password = "Password",
+                    Address = "Address",
+                    Country = "LT",
+                    City = "City",
+                    PostalCode = "123456"
+                });
+                context.SaveChanges();
+            }
+            using (var context = new WebDbContext(options))
+            {
+                var service = new UserService(context);
+                var userNew = await service.GetUserById(id);
+
+                Assert.Equal("Test1", userNew.FullName);
             }
         }
     }
