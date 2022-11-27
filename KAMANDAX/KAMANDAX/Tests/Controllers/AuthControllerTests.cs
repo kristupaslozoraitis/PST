587c6340bf10bdf6a040f3babf2b89dc60d61dfb
erikasnar <naerikas@gmail.com>
2022-11-27 15:40:20 +0200

    Adding generated tests

---
+++ KAMANDAX/KAMANDAX/Tests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using KAMANDAX.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAMANDAX.Controllers.Tests
+{
+    public class AuthControllerTests
+    {
+        [Fact()]
+        public void AuthControllerTest()
+        {
+
+        }
+
+        [Fact()]
+        public void RegisterTest()
+        {
+
+        }
+
+        [Fact()]
+        public void LoginTest()
+        {
+
+        }
+
+        [Fact()]
+        public void RefreshTest()
+        {
+
+        }
+
+        [Fact()]
+        public void LogoutTest()
+        {
+
+        }
+    }
+}
